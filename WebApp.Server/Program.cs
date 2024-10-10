25ba20057630ba54fc0f2c2b43673e28682b247b
Chaerul Rizky <riz.chaerul@gmail.com>
2024-10-11 01:08:55 +0700

    configure HttpClient

--- WebApp.Server/Program.cs
+++ WebApp.Server/Program.cs
@@ -8,7 +8,7 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        
+
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
@@ -17,8 +17,17 @@ public class Program
         {
             // Set the default tracking behavior to no tracking.
             options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            options.UseSqlServer(builder.Configuration.GetConnectionString("Application"));
+            options.UseSqlServer(builder.Configuration.GetConnectionString(AppConstants.DatabaseConstants.Application));
         });
+
+        builder.Services.AddHttpClient(
+            AppConstants.HttpClientConstants.DefaultClient,
+            client =>
+            {
+                client.Timeout = TimeSpan.FromSeconds(builder.Configuration.GetValue<int>(AppConstants.HttpClientConstants.DefaultClientTimeout));
+                client.DefaultRequestHeaders.Add("X-API-KEY", builder.Configuration[AppConstants.HttpClientConstants.DefaultClientApiKey]);
+            }
+        );
 
         var app = builder.Build();
 
---
+++ WebApp.Shared/AppConstants.cs
@@ -0,0 +1,14 @@
+public static class AppConstants
+{
+    public static class DatabaseConstants
+    {
+        public const string Application = "Application";
+    }
+
+    public static class HttpClientConstants
+    {
+        public const string DefaultClient = "DefaultClient";
+        public const string DefaultClientApiKey = "HttpClients:DefaultClient:ApiKey";
+        public const string DefaultClientTimeout = "HttpClients:DefaultClient:Timeout";
+    }
+}
