d18d857fd8ac8ffe3afeb0a4b02ecf3a12b33567
Chaerul Rizky <riz.chaerul@gmail.com>
2024-10-11 01:24:22 +0700

    add serilog

--- WebApp.Server/Program.cs
+++ WebApp.Server/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
 using WebApp.DB.Entities;
 
 namespace WebApp.Server;
@@ -7,39 +9,65 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateBootstrapLogger();
 
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
+        try
         {
-            // Set the default tracking behavior to no tracking.
-            options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            options.UseSqlServer(builder.Configuration.GetConnectionString(AppConstants.DatabaseConstants.Application));
-        });
+            Log.Information("Starting web application");
 
-        builder.Services.AddHttpClient(
-            AppConstants.HttpClientConstants.DefaultClient,
-            client =>
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddSerilog((services, lc) => lc.ReadFrom.Configuration(builder.Configuration).ReadFrom.Services(services));
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
             {
-                client.Timeout = TimeSpan.FromSeconds(builder.Configuration.GetValue<int>(AppConstants.HttpClientConstants.DefaultClientTimeout));
-                client.DefaultRequestHeaders.Add("X-API-KEY", builder.Configuration[AppConstants.HttpClientConstants.DefaultClientApiKey]);
+                // Set the default tracking behavior to no tracking.
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                options.UseSqlServer(builder.Configuration.GetConnectionString(AppConstants.DatabaseConstants.Application));
+            });
+
+            builder.Services.AddHttpClient(
+                AppConstants.HttpClientConstants.DefaultClient,
+                client =>
+                {
+                    client.Timeout = TimeSpan.FromSeconds(
+                        builder.Configuration.GetValue<int>(AppConstants.HttpClientConstants.DefaultClientTimeout)
+                    );
+                    client.DefaultRequestHeaders.Add(
+                        "X-API-KEY",
+                        builder.Configuration[AppConstants.HttpClientConstants.DefaultClientApiKey]
+                    );
+                }
+            );
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
-        );
 
-        var app = builder.Build();
+            app.UseAuthorization();
+            app.MapControllers();
 
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            app.Run();
         }
-
-        app.UseAuthorization();
-        app.MapControllers();
-
-        app.Run();
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Application terminated unexpectedly");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
