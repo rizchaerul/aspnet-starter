5901826e822c59214f782d46452124b350df18e6
Chaerul Rizky <riz.chaerul@gmail.com>
2024-10-22 22:35:17 +0700

    format codes

--- WebApp.NextClient/src/pages/index.page.tsx
+++ WebApp.NextClient/src/pages/index.page.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@/components/ui/button";
 import { NextPageWithLayout } from "@/types/layout";
 
 const IndexPage: NextPageWithLayout = () => {
     return (
         <div>
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
-            {/* <Button>Button</Button> */}
+            <Button>Button</Button>
         </div>
     );
 };
